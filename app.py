8d446e018be4f3f08c10f69b33631836e831746c
Metanon J <imetanon@gmail.com>
2020-10-01 21:51:20 +0700

    add confirm template

--- app.py
+++ app.py
@@ -44,5 +44,38 @@ def get_buttons_template():
     return resp
 
 
+@app.route('/confirm-template')
+def get_confirm_template():
+    data = {
+        "line_payload": [
+            {
+                "type": "template",
+                "altText": "this is a confirm template",
+                "template": {
+                    "type": "confirm",
+                    "actions": [
+                        {
+                            "type": "message",
+                            "label": "Yes",
+                            "text": "Yes"
+                        },
+                        {
+                            "type": "message",
+                            "label": "No",
+                            "text": "No"
+                        }
+                    ],
+                    "text": "Continue?"
+                }
+            }
+        ]
+    }
+
+    js = json.dumps(data)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['reply-by-object'] = True
+    return resp
+
+
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
